3e2c4bd9e8b0fd9d680ef8dc464a68b97540f974
hossein <abdollahzadeh.hossein@gmail.com>
2023-01-31 12:11:22 +0330

    index categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,19 @@ class Category extends Model
     use HasFactory;
     protected $table = "categories";
     protected $guarded = [];
+
+    public function getIsActiveAttribute($is_active)
+    {
+        return $is_active ? 'فعال' : 'غیرفعال';
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -10,10 +10,10 @@
 
         <div class="col-xl-12 col-md-12 mb-4 p-md-5 bg-white">
             <div class="mb-4 d-flex justify-content-between">
-                <h5 class="font-weight-bold">لیست ویژگی ها ({{ $attributes->total() }})</h5>
-                <a class="btn btn-sm btn-outline-primary" href="{{route('admin.attributes.create')}}">
+                <h5 class="font-weight-bold">لیست دسته بندی ها ({{ $categories->total() }})</h5>
+                <a class="btn btn-sm btn-outline-primary" href="{{route('admin.categories.create')}}">
                     <i class="fa fa-plus"></i>
-                    ایجاد ویژگی
+                    ایجاد دسته بندی
                 </a>
             </div>
             <div>
@@ -22,20 +22,36 @@
                     <tr>
                         <th>#</th>
                         <th>نام</th>
+                        <th>نام انگلیسی</th>
+                        <th>والد</th>
+                        <th>وضعیت</th>
                         <th>عملیات</th>
                     </tr>
 
                     </thead>
                     <tbody>
-                    @foreach($attributes as $key=>$attribute)
+                    @foreach($categories as $key=>$category)
                         <tr>
-                            <th>{{$attributes->firstItem() + $key}}</th>
-                            <th>{{$attribute->name}}</th>
+                            <th>{{$categories->firstItem() + $key}}</th>
+                            <th>{{$category->name}}</th>
+                            <th>{{$category->slug}}</th>
+                            <th>
+                                @if($category->parent_id == 0)
+                                    -
+                                @else
+                                {{$category->parent->name}}
+                                @endif
+                            </th>
+                            <th>
+                            <span class="{{$category->getRawOriginal('is_active') ? 'text-success': 'text-danger'}}">
+                                    {{$category->is_active}}
+                                </span>
+                            </th>
                             <th>
                                 <a class="btn btn-sm btn-outline-success"
-                                   href="{{route('admin.attributes.show', $attribute->id)}}">نمایش</a>
+                                   href="{{route('admin.categories.show', $category->id)}}">نمایش</a>
                                 <a class="btn btn-sm btn-outline-info mr-3"
-                                   href="{{route('admin.attributes.edit', $attribute->id)}}">ویرایش</a>
+                                   href="{{route('admin.categories.edit', $category->id)}}">ویرایش</a>
                             </th>
                         </tr>
                     @endforeach
