eca017c74b83c36a83f5c23e8124909891a017e5
hossein <abdollahzadeh.hossein@gmail.com>
2023-02-13 20:22:46 +0330

    show banner in home page

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -11,6 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         $sliders = Banner::query()->where('type', 'slider')->where('is_active', 1)->orderBy('priority')->get();
-        return view('home.index', compact('sliders'));
+        $indexTopBanners = Banner::query()->where('type', 'index-top')->where('is_active', 1)->orderBy('priority')->get();
+        $indexBottomBanners = Banner::query()->where('type', 'index-bottom')->where('is_active', 1)->orderBy('priority')->get();
+        return view('home.index', compact('sliders', 'indexTopBanners', 'indexBottomBanners'));
     }
 }
