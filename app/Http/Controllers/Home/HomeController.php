7823ca2ac29e15024ecdcc4387665418f619346a
hossein <abdollahzadeh.hossein@gmail.com>
2023-02-13 19:55:37 +0330

    show slider in home

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $sliders = Banner::query()->where('type', 'slider')->where('is_active', 1)->orderBy('priority')->get();
+        return view('home.index', compact('sliders'));
     }
 }
