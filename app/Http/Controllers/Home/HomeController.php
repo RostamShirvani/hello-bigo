88474f17eec4828d712a084675cdd59f3b7af460
hossein <abdollahzadeh.hossein@gmail.com>
2023-02-16 16:16:21 +0330

    index page of shop

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Banner;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,6 +14,8 @@ class HomeController extends Controller
         $sliders = Banner::query()->where('type', 'slider')->where('is_active', 1)->orderBy('priority')->get();
         $indexTopBanners = Banner::query()->where('type', 'index-top')->where('is_active', 1)->orderBy('priority')->get();
         $indexBottomBanners = Banner::query()->where('type', 'index-bottom')->where('is_active', 1)->orderBy('priority')->get();
-        return view('home.index', compact('sliders', 'indexTopBanners', 'indexBottomBanners'));
+        $products = Product::query()->where('is_active', 1)->get()->take(5);
+
+        return view('home.index', compact('sliders', 'indexTopBanners', 'indexBottomBanners', 'products'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
@@ -11,6 +12,8 @@ class Product extends Model
     use HasFactory, sluggable;
     protected $table = "products";
     protected $guarded = [];
+    protected $appends = ['check_quantity', 'check_sale', 'check_price'];
+
     /**
      * Return the sluggable configuration array for this model.
      *
@@ -23,6 +26,31 @@ class Product extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    public function getCheckQuantityAttribute()
+    {
+        return $this->variations()->where('quantity', '>', 0)->first() ?? 0;
+    }
+
+    public function getCheckSaleAttribute()
+    {
+        return $this->variations()
+                ->where('quantity', '>', 0)
+                ->where('sale_price', '!=', null)
+                ->where('date_on_sale_from', '<', Carbon::now())
+                ->where('date_on_sale_to', '>', Carbon::now())
+                ->orderBy('sale_price')
+                ->first() ?? false;
+    }
+
+    public function getCheckPriceAttribute()
+    {
+        return $this->variations()
+                ->where('quantity', '>', 0)
+                ->where('price', '!=', null)
+                ->orderBy('sale_price')
+                ->first() ?? false;
     }
 
     public function getIsActiveAttribute($is_active)
@@ -58,5 +86,10 @@ class Product extends Model
     public function images()
     {
         return $this->hasMany(ProductImage::class);
+    }
+
+    public function rates()
+    {
+        return $this->hasMany(ProductRate::class);
     }
 }
