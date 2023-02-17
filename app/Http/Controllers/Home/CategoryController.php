b887da0b057db46584d589f51326fedb3f1e6eeb
hossein <abdollahzadeh.hossein@gmail.com>
2023-02-17 07:00:01 +0330

    get attributes & variation values of category

--- app/Http/Controllers/Home/CategoryController.php
+++ app/Http/Controllers/Home/CategoryController.php
@@ -10,6 +10,9 @@ class CategoryController extends Controller
 {
     public function show(Category $category)
     {
-        return view('home.categories.show', compact('category'));
+        $attributes = $category->attributes()->where('is_filter', 1)->with('values')->get();
+        $variation = $category->attributes()->where('is_variation', 1)->with('variationValues')->first();
+
+        return view('home.categories.show', compact('category', 'attributes', 'variation'));
     }
 }
