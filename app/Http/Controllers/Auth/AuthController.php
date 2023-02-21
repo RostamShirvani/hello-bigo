57c969ca0dd0a8b723915a9610927192b25c0955
hossein <abdollahzadeh.hossein@gmail.com>
2023-02-21 07:32:23 +0330

    login or register with google account

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
@@ -20,5 +23,19 @@ class AuthController extends Controller
         }catch (\Exception $exception){
             return redirect()->route('login');
         }
+        $user = User::query()->where('email', $socialite_user->getEmail())->first();
+        if(!$user){
+            $user = User::create([
+               'name' => $socialite_user->getName(),
+               'provider_name' => $provider,
+               'avatar' => $socialite_user->getAvatar(),
+               'email' => $socialite_user->getEmail(),
+                'password' => Hash::make($socialite_user->getId()),
+                'email_verified_at' => Carbon::now()
+            ]);
+        }
+        auth()->login($user, $remember = true);
+        alert()->success('تشکر', 'ورود شما موفقیت آمیز بود.')->persistent('حله');
+        return redirect()->route('home.index');
     }
 }
