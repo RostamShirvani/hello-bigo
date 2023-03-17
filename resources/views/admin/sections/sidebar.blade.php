d295ff81bfa8b9f8eb0fe352eba464057b1a2a94
hossein <abdollahzadeh.hossein@gmail.com>
2023-03-17 18:09:21 +0330

    example use middleware for permission & role

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -55,7 +55,7 @@
     </li>
 
 {{--    @can('create_product')--}}
-    @role('product_management')
+{{--    @role('product_management')--}}
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProducts"
                aria-expanded="true"
@@ -73,7 +73,7 @@
                 </div>
             </div>
         </li>
-    @endrole
+{{--    @endrole--}}
 {{--    @endcan--}}
 
 <!-- Divider -->
