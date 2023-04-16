861d32cac5ec56bb56a18069de51e6b39083df32
hossein <abdollahzadeh.hossein@gmail.com>
2023-04-16 20:15:14 +0330

    comment @vite for production

--- resources/views/home/layouts/home.blade.php
+++ resources/views/home/layouts/home.blade.php
@@ -14,7 +14,7 @@
     <!-- Custom styles for this template-->
     <link href="{{asset('/css/home.css')}}" rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/admin/admin.js', 'resources/js/home/home.js'])
+{{--    @vite(['resources/css/app.css', 'resources/js/admin/admin.js', 'resources/js/home/home.js'])--}}
 
     @yield('style')
 
