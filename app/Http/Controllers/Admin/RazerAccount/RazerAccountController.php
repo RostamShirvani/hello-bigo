ae2532fc97a9b77f7a73ebdfa6c9742b988b7e40
technosun <abdollahzadeh.hossein@gmail.com>
2024-08-31 17:47:33 +0330

    Set manual_updated_at on create new razerAccount

--- app/Http/Controllers/Admin/RazerAccount/RazerAccountController.php
+++ app/Http/Controllers/Admin/RazerAccount/RazerAccountController.php
@@ -43,12 +43,15 @@ class RazerAccountController extends Controller
         $validate_data = Validator::make(request()->all(), [
             'razer_id' => 'required',
             'email_address' => 'required|email',
+            'location' => 'nullable|string',
             'charge_balance' => 'required|numeric',
             'charge_ceiling' => 'nullable|numeric', // Allow charge_ceiling to be optional
         ])->validated();
 
         $validate_data['charge_ceiling'] = (int)$validate_data['charge_ceiling'] > 0 ? (int)$validate_data['charge_ceiling'] : 90000;
         $validate_data['charge_ceil_flag'] = (int)RazerAccount::min('charge_ceil_flag');
+
+        $validate_data['manual_updated_at'] = now();
 
         RazerAccount::create($validate_data);
 
