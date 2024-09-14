d63ce97aac053bade9f21e5930d4af8c409d30eb
technosun <abdollahzadeh.hossein@gmail.com>
2024-09-14 16:54:00 +0330

    Change footer of home

--- resources/views/home/sections/footer.blade.php
+++ resources/views/home/sections/footer.blade.php
@@ -1,38 +1,57 @@
 <footer class="footer-area bg-paleturquoise" style="direction: rtl;">
-    <div class="container">
-        <div class="footer-top text-center pt-45 pb-45">
-            <nav>
-                <ul>
-                    <li><a href="{{route('home.index')}}">صفحه ای اصلی </a></li>
-                    <li><a href="#">فروشگاه </a></li>
-                    <li><a href="{{route('home.contact-us')}}">تماس با ما </a></li>
-                    <li><a href="{{route('home.about-us')}}">ارتباط با ما </a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-    <div class="footer-bottom border-top-1 pt-20">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-4 col-md-5 col-12">
-                    <div class="footer-social pb-20">
-                        <a href="#">اینستاگرام</a>
-                        <a href="#">تویتر</a>
-                        <a href="#">فیسبوک</a>
-                        <a href="#">لینکدین</a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-12">
-                    <div class="copyright text-center pb-20">
-                        <p>Copyright © eCommerce</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-3 col-12">
-                    <div class="payment-mathod pb-20">
-                        <a href="#"><img src="#" alt=""/></a>
-                    </div>
-                </div>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="text-justify">
+                    <span class="mb-4" style="font-size: large">دربـــاره بیـــــگو پــلاس بـیشتر بــدانید</span><br><br>
+                    فروشگاه اینترنتی Bigoplus امن ترین و سریع ترین فروشگاه اینترنتی خرید الماس بیگولایو و لایکی را به صورت کاملا اتوماتیک در اختیار شما مشتریان گرامی قرار داده است .اکانت  شما پس از پرداخت در عرض کمتر از ده ثانیه توسط سیستم اتوماتیک  ما شارژ خواهد شد و نگرانی از بابت ثبت و خرید الماس خود نداشته باشید. پشتیبانان تیم بیگو پلاس از طریق واتساپ همواره در خدمت شما مشتریان گرامی میباشد
+                </p>
+            </div>
+            <div class="col-md-6">
+                <span style="font-weight: bold;">پشتیانی فقط از طریق واتساپ</span>
+                <br>
+                09224525766
+                <br><br>
+                ساعت پاسخگویی
+                <br><br>
+                ساعت 8 تا 24
+                <br><br>
+                روزهای تعطیل 10 تا 24
             </div>
         </div>
+{{--        <div class="footer-top text-center pt-45 pb-45">--}}
+{{--            <nav>--}}
+{{--                <ul>--}}
+{{--                    <li><a href="{{route('home.index')}}">صفحه ای اصلی </a></li>--}}
+{{--                    <li><a href="#">فروشگاه </a></li>--}}
+{{--                    <li><a href="{{route('home.contact-us')}}">تماس با ما </a></li>--}}
+{{--                    <li><a href="{{route('home.about-us')}}">ارتباط با ما </a></li>--}}
+{{--                </ul>--}}
+{{--            </nav>--}}
+{{--        </div>--}}
+    </div>
+    <div class="footer-bottom border-top-1 pt-20">
+{{--        <div class="container">--}}
+{{--            <div class="row align-items-center">--}}
+{{--                <div class="col-lg-4 col-md-5 col-12">--}}
+{{--                    <div class="footer-social pb-20">--}}
+{{--                        <a href="#">اینستاگرام</a>--}}
+{{--                        <a href="#">تویتر</a>--}}
+{{--                        <a href="#">فیسبوک</a>--}}
+{{--                        <a href="#">لینکدین</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-4 col-12">--}}
+{{--                    <div class="copyright text-center pb-20">--}}
+{{--                        <p>Copyright © eCommerce</p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-3 col-12">--}}
+{{--                    <div class="payment-mathod pb-20">--}}
+{{--                        <a href="#"><img src="#" alt=""/></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 </footer>
