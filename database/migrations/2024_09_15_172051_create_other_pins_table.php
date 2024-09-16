fb055ced01b2643a9407f1fa6ca375cd8dc307ea
Hossein <abdollahzadeh.hossein@gmail.com>
2024-09-16 10:48:46 +0330

    Refactor migration create_other_pins_table

--- database/migrations/2024_09_15_172051_create_other_pins_table.php
+++ database/migrations/2024_09_15_172051_create_other_pins_table.php
@@ -17,16 +17,20 @@ return new class extends Migration
             $table->id();
             $table->string('title')->nullable();
             $table->string('pin');
-            $table->tinyInteger('type');
+            $table->tinyInteger('app_type');
             $table->decimal('amount', 25, 2);
             $table->integer('value');
             $table->tinyInteger('status')->default(0); // Adjust the default as needed
             $table->tinyInteger('state')->default(1); // Adjust the default as needed
+            $table->foreignId('order_id')->nullable()->references('id')->on('orders')->onDelete('SET NULL');
+            $table->foreignId('order_item_id')->nullable()->references('id')->on('order_items')->onDelete('SET NULL');
+            $table->string('tracking_code', 50)->nullable();
             $table->string('used_by_mobile', 15)->nullable();
+            $table->string('description')->nullable();
             $table->timestamp('used_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->string('used_by')->nullable();
+            $table->foreignId('used_by')->nullable()->references('id')->on('users')->onDelete('SET NULL');
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->unsignedBigInteger('deleted_by')->nullable();
